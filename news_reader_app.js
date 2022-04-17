2c03eaccb66f9cfe46e2643422caa8af8720e573
yasscht <73077126+yasscht@users.noreply.github.com>
2022-04-17 12:38:59 +0000

    executing the first command

--- news_reader_app.js
+++ news_reader_app.js
@@ -1,2 +1,5 @@
 // Use this sample to create your own voice commands
 intent("What does this app do?","What can i do here?",reply("This is a news project."))
+intent('Start a command',(p)=>{
+    p.play({command:'test'})
+})
